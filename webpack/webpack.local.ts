c860a4a7482eecaaeef4f4be76c30d86830c4790
abhishek sreepal <abhisheksreepal@abhisheks-MacBook-Air.local>
2024-03-16 00:08:43 +0530

    created seperate local webpack task for running locally.Configuring 404 page for github page

--- webpack/webpack.local.ts
+++ webpack/webpack.local.ts
@@ -1,17 +1,27 @@
 import type { Configuration } from "webpack";
 import path from 'path';
-import commonWebpackConfig from './webpack.common';
+import devConfig from './webpack.dev';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import CopyPlugin from 'copy-webpack-plugin'
 import merge from 'webpack-merge';
 import type {Configuration as DevServerConfiguration} from 'webpack-dev-server'
 
-const config: Configuration = merge(commonWebpackConfig, {
-    mode: 'development',
-    devtool: 'eval-cheap-module-source-map',
+const config: Configuration = merge(devConfig, {
     output: {
         path: path.resolve(__dirname,'../target'),
         filename: '[name].bundle.js',
+        publicPath: 'http://localhost:9000/mine-sweeper/'
+    },
+    devServer: {
+        port: 9000,
+        historyApiFallback: {
+            index: 'http://localhost:9000/mine-sweeper/'
+        },
+        
+        static: {
+            directory: path.resolve(__dirname,'../target')
+        },
+        compress: true,
     }
     
 },
--- webpack/webpack.prod.ts
+++ webpack/webpack.prod.ts
@@ -2,19 +2,26 @@ import { Configuration } from "webpack";
 import path from 'path';
 import commonWebpackConfig from './webpack.common';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
+import CopyPlugin from 'copy-webpack-plugin'
+
 import merge from 'webpack-merge';
 
 const config: Configuration = merge(commonWebpackConfig, {
     mode: 'production',
     output: {
         path: path.resolve(__dirname,'../target'),
-        filename: '[name].bundle.js'
+        filename: '[name].bundle.js',
     }
 },
 {
     plugins: [
         new HtmlWebpackPlugin({
             template: './public/index.html'
+        }),
+        new CopyPlugin({
+            patterns: [
+                {from : path.resolve(__dirname,'../public/404.html'), to: path.resolve(__dirname,'../target/404.html')}
+            ]
         })
     ]
 })
