2dd8f4e08c022f38c5fed2ef06b8d54e39d8f3c5
abhishek sreepal <abhisheksreepal@abhisheks-MacBook-Air.local>
2024-03-16 16:11:00 +0530

    Configured global style for react app

---
+++ src/App.styles.ts
@@ -0,0 +1,16 @@
+import { css } from "@emotion/react";
+
+const styles = css`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html {
+    width: 100%;
+    height: 100%;
+  }
+`;
+
+export default styles;
