731f8e9b074acf8b743ec2051bb36c7f1f096696
abhishek sreepal <abhisheksreepal@abhisheks-MacBook-Air.local>
2024-03-16 16:11:44 +0530

    Created Default pages

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,34 @@
-import React from "react";
+import React, { useContext } from "react";
+import HeadingComponent from "../components/Heading.component";
+import { observer } from "mobx-react-lite";
+import StoreContext from "../contexts/store.context";
+import GameComponent from "../components/Game.component";
+import WelcomeComponent from "../components/Welcome.component";
+import ModalComponent from "../components/Modal.component";
+import ConfigurationComponent from "../components/Configuration.component";
 
-export default function () {
-  return "TODO";
-}
+const HomePage = observer(() => {
+  const { rootStore } = useContext(StoreContext);
+
+  return (
+    <React.Fragment>
+      <HeadingComponent></HeadingComponent>
+      {rootStore.uiStore.showGameView ? (
+        <GameComponent></GameComponent>
+      ) : (
+        <WelcomeComponent></WelcomeComponent>
+      )}
+      {rootStore.uiStore.showLaunchDialog ? (
+        <ModalComponent
+          isOpen={true}
+          onClose={() => {
+            rootStore.uiStore.showLaunchDialog = false;
+          }}
+        >
+          <ConfigurationComponent></ConfigurationComponent>
+        </ModalComponent>
+      ) : null}
+    </React.Fragment>
+  );
+});
+export default HomePage;
